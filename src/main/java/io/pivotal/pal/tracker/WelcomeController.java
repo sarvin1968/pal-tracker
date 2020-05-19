0ee0b4b10ca64ef266bfb8df071005dc46a8b74c
Ubuntu <ubuntu@ip-172-31-94-228.ec2.internal>
2020-05-19 18:40:34 +0000

    External Configuration Lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,26 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private final String externalConfigVal;
+
+
+    public WelcomeController(@Value("${welcome.message}")String externalConfigVal) {
+        this.externalConfigVal = externalConfigVal;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return externalConfigVal;
     }
+
+
+
+
 
 }
